992ea994c9cf6d5fdf66f92c3344c89f10ffb29f
lablelan <lablelan@163.com>
2025-07-08 23:01:09 +0800

    中文菜单

--- packages/bruno-electron/src/app/menu-template.js
+++ packages/bruno-electron/src/app/menu-template.js
@@ -6,36 +6,36 @@ const aboutBruno = require('./about-bruno');
 
 const template = [
   {
-    label: 'Collection',
+    label: '集合',
     submenu: [
       {
-        label: 'Open Collection',
+        label: '打开集合',
         click() {
           ipcMain.emit('main:open-collection');
         }
       },
       {
-        label: 'Open Recent',
+        label: '打开最近文件',
         role: 'recentdocuments',
         visible: os.platform() == 'darwin',
         submenu: [
           {
-            label: 'Clear Recent',
+            label: '清除最近文件',
             role: 'clearrecentdocuments'
           }
         ]
       },
       {
-        label: 'Preferences',
+        label: '偏好设置',
         accelerator: 'CommandOrControl+,',
         click() {
           ipcMain.emit('main:open-preferences');
         }
       },
       { type: 'separator' },
-      { role: 'quit' },
+      { role: 'quit', label: '退出' },
       {
-        label: 'Force Quit',
+        label: '强制退出',
         click() {
           process.exit();
         }
@@ -43,41 +43,43 @@ const template = [
     ]
   },
   {
-    label: 'Edit',
+    label: '编辑',
     submenu: [
-      { role: 'undo' },
-      { role: 'redo' },
+      { role: 'undo', label: '撤销' },
+      { role: 'redo', label: '重做' },
       { type: 'separator' },
-      { role: 'cut' },
-      { role: 'copy' },
-      { role: 'paste' },
-      { role: 'selectAll' },
+      { role: 'cut', label: '剪切' },
+      { role: 'copy', label: '复制' },
+      { role: 'paste', label: '粘贴' },
+      { role: 'selectAll', label: '全选' },
       { type: 'separator' },
-      { role: 'hide' },
-      { role: 'hideOthers' }
+      { role: 'hide', label: '隐藏' },
+      { role: 'hideOthers', label: '隐藏其他' }
     ]
   },
   {
-    label: 'View',
+    label: '视图',
     submenu: [
-      { role: 'toggledevtools' },
+      { role: 'toggledevtools', label: '切换开发者工具' },
       { type: 'separator' },
-      { role: 'resetzoom' },
-      { role: 'zoomin' },
-      { role: 'zoomout' },
+      { role: 'resetzoom', label: '重置缩放' },
+      { role: 'zoomin', label: '放大' },
+      { role: 'zoomout', label: '缩小' },
       { type: 'separator' },
-      { role: 'togglefullscreen' }
+      { role: 'togglefullscreen', label: '切换全屏' }
     ]
   },
   {
     role: 'window',
-    submenu: [{ role: 'minimize' }, { role: 'close', accelerator: 'CommandOrControl+Shift+Q' }]
+    label: '窗口',
+    submenu: [{ role: 'minimize', label: '最小化' }, { role: 'close', label: '关闭', accelerator: 'CommandOrControl+Shift+Q' }]
   },
   {
     role: 'help',
+    label: '帮助',
     submenu: [
       {
-        label: 'About Bruno',
+        label: '关于Bruno',
         click: () => {
           const aboutWindow = new BrowserWindow({
             width: 350,
@@ -89,9 +91,9 @@ const template = [
           aboutWindow.loadURL(`data:text/html;charset=utf-8,${encodeURIComponent(aboutBruno({version}))}`);
         }
       },
-      { label: 'Documentation', click: () => ipcMain.emit('main:open-docs') }
+      { label: '文档', click: () => ipcMain.emit('main:open-docs') }
     ]
   }
 ];
 
-module.exports = template;
+module.exports = template;  
